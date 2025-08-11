87d58809b940684107bd5feb5d5aef6a1c9212db
David Monge <dmongech@emeal.nttdata.com>
2025-08-11 12:19:54 +0200

    Add supabase-client service to server

--- src/app/core/services/supabase-client.service.ts
+++ src/app/core/services/supabase-client.service.ts
@@ -1,6 +1,13 @@
+// src/app/core/supabase.client.ts
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
 
-export function getSupabaseClient(): SupabaseClient {
-  return createClient(environment.supabaseUrl, environment.supabaseKey);
+let client: SupabaseClient | null = null;
+
+export async function getSupabaseClient(): Promise<SupabaseClient> {
+  if (!client) {
+    const mod = await import('@supabase/supabase-js');
+    client = mod.createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+  return client;
 }
